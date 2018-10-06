18476eb6452ddebac8fec7ee659099ac9673b53b
carlosescom <carlos.escom.ipn@gmail.com>
2018-10-06 11:05:41 -0500

    Creo el contrato de Recaudación y dos funciones principales.

---
+++ contracts/RecaudacionTeleton.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.0;
+
+import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
+
+contract RecaudacionTeleton is Crowdsale {
+
+    function AsignarTeletokens(address beneficiario) public payable {
+        buyTokens(beneficiario);
+    }
+
+    function DonarTeletokens(uint256 cuantosTokens) public payable {
+        approve(msg.sender,cuantosTokens);
+    }
+}
