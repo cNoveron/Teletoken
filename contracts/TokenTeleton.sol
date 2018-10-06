71e6a01b9a7fb71472c31ad0ff03c63c7504ccc1
carlosescom <carlos.escom.ipn@gmail.com>
2018-10-06 08:06:30 -0500

    Actualizo el TeleToken.

--- contracts/TokenTeleton.sol
+++ contracts/TokenTeleton.sol
@@ -1,13 +1,14 @@
 pragma solidity ^0.4.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract TokenTeleton is StandardToken {
+contract TokenTeleton is StandardToken, Ownable {
   string public nombre = "TeleToken";
-  string public denominacion = "MXN para Teleton";
+  string public denominacion = "MXN donados al Teleton";
   uint public decimales = 2;
-  uint public oferta_total = 456013739;
-
+  uint public oferta_total = 456013739 * 10**uint(decimales);
+  
   constructor() public {
     totalSupply_ = oferta_total;
     balances[msg.sender] = oferta_total;
