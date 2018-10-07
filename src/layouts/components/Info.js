1b36cb089849ef85b3127f8a91bfc814a37c3388
Lenin Meza <merolhack@gmail.com>
2018-10-06 19:39:40 -0500

    :bug: fix: Info missing link

--- src/layouts/components/Info.js
+++ src/layouts/components/Info.js
@@ -11,27 +11,29 @@ class Menu extends Component {
     }
     render() {
         return (
-            <div className='tableauPlaceholder' id='viz1538870802008' style={{position: 'relative'}}>
-                <noscript>
-                    <a href='#'>
-                        <img alt='  LOGROS &amp; TESTIMONIOS. ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Lo&#47;Logrostestimonios&#47;Hoja1&#47;1_rss.png' style={{border: 'none'}} />
-                    </a>
-                </noscript>
-                <object className='tableauViz'>
-                    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-                    <param name='embed_code_version' value='3' />
-                    <param name='site_root' value='' />
-                    <param name='name' value='Logrostestimonios&#47;Hoja1' />
-                    <param name='tabs' value='no' />
-                    <param name='toolbar' value='yes' />
-                    <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Lo&#47;Logrostestimonios&#47;Hoja1&#47;1.png' />
-                    <param name='animate_transition' value='yes' />
-                    <param name='display_static_image' value='yes' />
-                    <param name='display_spinner' value='yes' />
-                    <param name='display_overlay' value='yes' />
-                    <param name='display_count' value='yes' />
-                    <param name='filter' value='publish=yes' />
-                </object>
+            <div id="info">
+                <div className='tableauPlaceholder' id='viz1538870802008' style={{position: 'relative'}}>
+                    <noscript>
+                        <a href='#'>
+                            <img alt='  LOGROS &amp; TESTIMONIOS. ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Lo&#47;Logrostestimonios&#47;Hoja1&#47;1_rss.png' style={{border: 'none'}} />
+                        </a>
+                    </noscript>
+                    <object className='tableauViz'>
+                        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+                        <param name='embed_code_version' value='3' />
+                        <param name='site_root' value='' />
+                        <param name='name' value='Logrostestimonios&#47;Hoja1' />
+                        <param name='tabs' value='no' />
+                        <param name='toolbar' value='yes' />
+                        <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Lo&#47;Logrostestimonios&#47;Hoja1&#47;1.png' />
+                        <param name='animate_transition' value='yes' />
+                        <param name='display_static_image' value='yes' />
+                        <param name='display_spinner' value='yes' />
+                        <param name='display_overlay' value='yes' />
+                        <param name='display_count' value='yes' />
+                        <param name='filter' value='publish=yes' />
+                    </object>
+                </div>
             </div>
         );
     }
