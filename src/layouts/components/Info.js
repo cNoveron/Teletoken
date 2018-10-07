b54941fd2bac4afdbbeca7ad6fb7351201546c09
Lenin Meza <merolhack@gmail.com>
2018-10-06 19:40:55 -0500

    :bug: fix: HTML Codes removed

--- src/layouts/components/Info.js
+++ src/layouts/components/Info.js
@@ -15,17 +15,17 @@ class Menu extends Component {
                 <div className='tableauPlaceholder' id='viz1538870802008' style={{position: 'relative'}}>
                     <noscript>
                         <a href='#'>
-                            <img alt='  LOGROS &amp; TESTIMONIOS. ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Lo&#47;Logrostestimonios&#47;Hoja1&#47;1_rss.png' style={{border: 'none'}} />
+                            <img alt='LOGROS y TESTIMONIOS.' src='https://public.tableau.com/static/images/Lo/Logrostestimonios/Hoja1/1_rss.png' style={{border: 'none'}} />
                         </a>
                     </noscript>
                     <object className='tableauViz'>
                         <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
                         <param name='embed_code_version' value='3' />
                         <param name='site_root' value='' />
-                        <param name='name' value='Logrostestimonios&#47;Hoja1' />
+                        <param name='name' value='Logrostestimonios/Hoja1' />
                         <param name='tabs' value='no' />
                         <param name='toolbar' value='yes' />
-                        <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Lo&#47;Logrostestimonios&#47;Hoja1&#47;1.png' />
+                        <param name='static_image' value='https://public.tableau.com/static/images/Lo/Logrostestimonios/Hoja1/1.png' />
                         <param name='animate_transition' value='yes' />
                         <param name='display_static_image' value='yes' />
                         <param name='display_spinner' value='yes' />
